412207d87695e2625e2d5f8cb3aaafac5b13bf72
Besufekad <besufekadtessema@gmail.com>
2024-05-08 00:23:47 -0700

    removed API key

--- assets/utils/getChatGPT.js
+++ assets/utils/getChatGPT.js
@@ -3,7 +3,7 @@ myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Accept", "application/json");
 myHeaders.append(
   "Authorization",
-  "Bearer sk-proj-ljz6025793pfNLudQnMST3BlbkFJ6zZnPsoFMSpsJU8Av97Z"
+  "Bearer API key here"
 );
 myHeaders.append(
   "Cookie",
